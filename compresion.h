d663f8ba079a88eb3a7636ae7acc662fc2c7ee93
Jsilva211 <132105240+Jsilva211@users.noreply.github.com>
2024-06-23 21:00:01 -0400

    comprimir por bloques
    ahora comprimir recibe de parametro un string con la ubicacion del archivo a comprimir

--- compresion.h
+++ compresion.h
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <fstream>
 #include <stdio.h>
 
 using namespace std;
@@ -11,19 +12,49 @@ class compresion {
 public:
 	vector<int> comprimir(const string& input) {
     vector<int> compressedResult;
-    Trie trie;
-    int inputSize = input.size();
-    string aux = input;
-
-
+    
     int j=0;
+    
+    
 
-    for(int i= 0;i< inputSize;i++){
+    ifstream file(input);
+    
+    
+    if (!file.is_open()) {
+        cerr << "Error abriendo el archivo." << endl;
+        compressedResult.push_back(0);
+        return compressedResult;
+    }
+    string line;
+    string block;
+    int abc=1;
+    while (file.peek() != EOF){
+        Trie trie;
+        while (getline(file, line) && block.size()<20000 ) {
+        block+=line;
+        
+        
+    }
+    
+
+
+
+    string aux=block;
+    
+
+    j=0;
+
+    for(int i= 0;i< block.size();i++){
+        
         if(aux.size()<1){
             break;
             }
+
         
         string aux2=aux.substr(0, 1);
+        
+
+        
         while( trie.startsWith(aux2)){
 
             j++;
@@ -31,6 +62,7 @@ public:
                     break;
                 }
             aux2=aux.substr(0, j+1);
+            
 
 
             }
@@ -40,13 +72,16 @@ public:
             compressedResult.push_back(aux[0]);
             compressedResult.push_back(0);
             aux=aux.substr(1);
+            
 
         }else{
-            size_t position = input.find(aux[0], 0);
+            size_t position = block.find(aux[0], 0);
+            
             compressedResult.push_back(position);
             compressedResult.push_back(j);
             if(aux.size()>j){
                 aux = aux.substr(j);
+                
 
             }else{
                 break;
@@ -57,7 +92,18 @@ public:
         j=0;
 
         }
+        compressedResult.push_back(0);
+        compressedResult.push_back(0);
+        block.clear();
+        trie.clear();
+        printf("bloque%d\n ", abc);
+        abc++;
+        
 
+
+    }
+    file.close();
+   
 
     return compressedResult;
     }
@@ -65,19 +111,31 @@ public:
     string descomprimir(const vector<int>& compressedResult){
 
         string originalString;
+        string currentblock;
         for(int i=0;i<compressedResult.size();i=i+2){
-            if(compressedResult[i + 1]==0){
-                char character = static_cast<char>(compressedResult[i]);
-                originalString += character;
-            
+
+            if(compressedResult[i]==0 && compressedResult[i+1]==0){
+                originalString += currentblock;
+                currentblock.clear();
 
             }else{
-                for(int j=0;j<compressedResult[i + 1];j++){
-                    char character = originalString[compressedResult[i]+j];
-                    originalString += character;
+                if(compressedResult[i + 1]==0){
+                char character = static_cast<char>(compressedResult[i]);
+                currentblock += character;
+            
 
+                }else{
+                     for(int j=0;j<compressedResult[i + 1];j++){
+                        char character = currentblock[compressedResult[i]+j];
+                        currentblock += character;
+
+                     }
                 }
+
             }
+
+
+            
         }
 
         return originalString;
